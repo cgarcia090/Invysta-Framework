174fc5466d9e8fa6b49d8efd00f8f3f6b6b7ac25
Cyril Garcia <garciacyrilivar5768@yahoo.com>
2021-03-27 16:23:49 -0700

    Spike/moved encryption to source extension

--- Sources/Invysta-Framework/Models/IdentifierManager.swift
+++ Sources/Invysta-Framework/Models/IdentifierManager.swift
@@ -8,29 +8,14 @@
 import Foundation
 import CommonCrypto
 
-open class Identifier {
-    public func SHA256(data: Data?) -> String {
-        guard let data = data else { return "SHA-err" }
-        var hash = [UInt8](repeating: 0,  count: Int(CC_SHA256_DIGEST_LENGTH))
-        data.withUnsafeBytes {
-            _ = CC_SHA256($0.baseAddress, CC_LONG(data.count), &hash)
-        }
-        
-        return Data(hash).map { String(format: "%02hhx", $0) }.joined()
-    }
-}
-
-open class IdentifierManager: Identifier {
+open class IdentifierManager {
     
     private let sources: [IdentifierSource]
         
-    private(set) var compiledSources: [String] = []
+    public private(set) var compiledSources: [String] = []
     
     public init(_ sources: [IdentifierSource]) {
-        
         self.sources = sources
-        
-        super.init()
         compiledSources = self.sources.compactMap({ $0.identifier() })
     }
     
@@ -46,6 +31,16 @@ open class IdentifierManager: Identifier {
         }
         
         return SHA256(data: caid.data(using: .ascii))
+    }
+    
+    private func SHA256(data: Data?) -> String {
+        guard let data = data else { return "SHA-err" }
+        var hash = [UInt8](repeating: 0,  count: Int(CC_SHA256_DIGEST_LENGTH))
+        data.withUnsafeBytes {
+            _ = CC_SHA256($0.baseAddress, CC_LONG(data.count), &hash)
+        }
+        
+        return Data(hash).map { String(format: "%02hhx", $0) }.joined()
     }
     
 }
--- Sources/Invysta-Framework/Models/IdentifierSource.swift
+++ Sources/Invysta-Framework/Models/IdentifierSource.swift
@@ -6,8 +6,22 @@
 //
 
 import Foundation
+import CommonCrypto
 
 public protocol IdentifierSource {
     var type: String { get set }
     func identifier() -> String?
+    func SHA256(data: Data?) -> String
+}
+
+extension IdentifierSource {
+    public func SHA256(data: Data?) -> String {
+        guard let data = data else { return "SHA-err" }
+        var hash = [UInt8](repeating: 0,  count: Int(CC_SHA256_DIGEST_LENGTH))
+        data.withUnsafeBytes {
+            _ = CC_SHA256($0.baseAddress, CC_LONG(data.count), &hash)
+        }
+        
+        return Data(hash).map { String(format: "%02hhx", $0) }.joined()
+    }
 }
